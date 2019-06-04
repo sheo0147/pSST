a2f3cf6f2decdc09dd8551e6770a686d95d259fe
HEO SeonMeyong <seirios@seirios.org>
2019-06-04 17:58:21 +0900

    format changed.

--- Functions/parse_yaml/parse_yaml.sh
+++ Functions/parse_yaml/parse_yaml.sh
@@ -1,53 +1,32 @@
-#! /bin/sh
-parse_yaml() {
-# Usage: parse_yaml configFile Prefix
+parse_yaml() { # Usage: parse_yaml file EnvPrefix
   [ ! -f "${1}" ] && echo "\"${1}\" not found." && exit 1
   [ -z "${2}" ] && echo "Must need prefix." && exit 1
   cat $1 | awk '
     {
-      sub(/[\t ]*#.*$/,"",$0);
+      sub(/[\t ]*#.*$/,"",$0)
       if(length($0) == 0) { next }
-      LINE=$0;
-      sub(/[\t ]+/,"",LINE);
-      INDENT=(length($0)-length(LINE))/2;
-      KEY=LINE; VALUE=LINE
-      sub(/:.*/,"",KEY);
-      sub(/[^:]*:[\t ]*/,"",VALUE);
+      LINE=$0; sub(/[\t ]+/,"",LINE)
+      INDENT=(length($0)-length(LINE))/2
+      KEY=LINE; sub(/:.*/,"",KEY)
+      VALUE=LINE; sub(/[^:]*:[\t ]*/,"",VALUE)
     
-      if( KEY ~ /^-[ ]*/ ) {} else {
-        VNAME[INDENT] = ("_")(KEY);
-      }
+      if( KEY ~ /^-[ ]*/ ) {} else { VNAME[INDENT] = ("_")(KEY) }
       for( i in VNAME ) {if ( i > INDENT ) {delete VNAME[i]}}
       if ( length(VALUE) > 0 ) {
         if( match(VALUE, /\[.*]$/)) {
           VALUE=substr(VALUE, RSTART+1, RLENGTH-2)
-          gsub( /,/ , " ", VALUE )
+          gsub( /,/, " ", VALUE )
           gsub( /^[\t ]+/, "", VALUE )
           gsub( /[\t ]+$/, "", VALUE )
         }
-        gsub( /"/, "\\\"" ,VALUE );
+        gsub( /"/, "\\\"" ,VALUE )
       }
-      vn=""
-      for (i=0; i <= INDENT; i++) {vn=(vn)(VNAME[i])}
+      vn=""; for (i=0; i <= INDENT; i++) {vn=(vn)(VNAME[i])}
       if( KEY ~ /^-[ ]*/ ) {
         gsub( /^-[ ]*/, "", KEY )
-        #printf("%s%s=\"${%s%s} \\\"%s\\\"\";\n", "'${2}'", vn, "'${2}'", vn, KEY);
-        printf("%s%s=\"${%s%s} %s\";\n", "'${2}'", vn, "'${2}'", vn, KEY);
+        printf("%s%s=\"${%s%s} %s\";\n", "'${2}'", vn, "'${2}'", vn, KEY)
       } else {
-        printf("%s%s=\"%s\";\n", "'${2}'", vn, VALUE);
+        printf("%s%s=\"%s\";\n", "'${2}'", vn, VALUE)
       }
     }'
-}
-
-read_eval_yaml_config() {
-# Usage: read_eval_yaml_config configFile Prefix Tempdir
-  [ ! -f "${1}" ] && echo "\"${1}\" not found." && exit 1
-  [ -z "${2}" ] && echo "Must need prefix." && exit 1
-  [ -z "${3}" ] && TMPDIR=${3} || TMPDIR=$(mktemp -d /tmp/parse.XXXXX)
-
-  parse_yaml ${1} ${2}
-echo "====="
-  parse_yaml ${1} ${2} > ${TMPDIR}/ptmp
-  . ${TMPDIR}/ptmp
-  rm -rf ${TMPDIR}
 }
