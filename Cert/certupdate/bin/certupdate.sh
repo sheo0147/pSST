24ccd2384ea93b5fdf54f00141f4fb25967b1be4
HEO SeonMeyong <seirios@seirios.org>
2019-07-01 21:43:27 +0900

    Rewrite for acme.sh.

--- Cert/certupdate/bin/certupdate.sh
+++ Cert/certupdate/bin/certupdate.sh
@@ -1,198 +1,313 @@
-#! /bin/sh
+#!/bin/sh
+##############################################################################
+# certupdate.sh
+#   Let's Encrypt free DV certificate updater.
+#   Author: HEO SeonMeyong (seirios@seirios.org)
+#   License: BSD 2 clause
+##############################################################################
 
-################################################################################
-# Prepareing
-################################################################################
+__VERSION="0.1"
+__TAB=$'\t'
 
-: ${DEBUG:=0}
-: ${DEBUG_ECHO:=0}
-: ${FORCE_DIST:=0}
-: ${DIST_ONLY:=0}
-: ${FORCE_UPDATE:=0}
+: ${__DBG:=0}					# Debug Information
+: ${__TEST:=0}					# Dry Run
+: ${__ACME:=1}					# Get or renew Certificate
+: ${__DIST:=1}					# Distribute
+: ${__FORCE:=0}					# Force update/distribute
 
-: ${DUSER:=""}
-: ${ACME_BASE:=""}
+: ${__DUSER:=""}				# Distribute username for ssh.
+: ${__HOME:="${PWD}"}				# Certificate home directory.
+: ${__WEBR:="${__HOME}/htdocs"}			# ACME WebRoot Directory.
+: ${__CONF:="${__HOME}/certupdate.conf"}	# configuration file.
+: ${__DOMF:="${__HOME}/domains.conf"}		# Domains configuration file.
+: ${__CAI:="LE"}				# CA issure.
+: ${__ACME_ACTION:="--issue"}			# ACME Action.
 
-[ -z ${DUSER} ] && echo "Please set distributers account name to DUSER" && exit 1
-[ -z ${ACME_BASE} ] && echo "Please set Base Directory to ACME_BASE environmenr" && exit 1
+__LOG="/dev/null"
+__CONF_TMP=""
+__DOMF_TMP=""
+__HOME_TMP=""
 
-while getopts DEdou __FLAG; do
-  case "${__FLAG}" in
-  D) # DEBUG
-     DEBUG=1
+##### functions definition.
+cu_debug() { # Debug output to stderr. ${1}:Messages / ${2}:Flag(Exit)
+  echo "DBG:${1}" >&2
+  echo "DBG:${1}" >> ${__LOG}
+  [ -n "${2}" ] && exit 1
+}
+
+cu_usage() { # Usage Message. force exit.
+  echo "${0} v.${__VERSION}"
+  echo "Usage: ${0} -adfrtv -C [config file] -D [domain file] -H [home dir]"
+  echo "	-a: not run acme process"
+  echo "	-d: not distribute certificate file"
+  echo "	-f: force run acme/distribute"
+  echo "	-t: ACME issue Process running on TEST mode)"
+  echo "	-r: renew certificate. (default issue)"
+  echo "	-V: Verbose/DEBUG"
+  echo "	-C [config file]: configuration file (Default: ./certupdate.conf)"
+  echo "	-D [domain file]: domain definition file (Default: ./domains.conf)"
+  echo "	-H [home dir]:    Cert file storages and work home (default: .)"
+  echo "	-W [WebRoot dir]: ACME WebRoot Dir (default: ./htdocs)"
+  exit 1
+}
+
+cu_count_arg() { # count argument
+  echo ${#}
+}
+
+cu_sort_uniq_arg() { # sort and uniq the argument
+  for i in $*; do
+    echo ${i}
+  done | sort | uniq | wc -l | sed "s/^[ ${__TAB}]*//"
+}
+
+##### Main #####
+
+### Get options.
+
+while getopts "adfrtVC:D:H:W:" __FLAG__; do
+  case "${__FLAG__}" in
+  a)	# not run ACME
+    __ACME=0
   ;;
-  E) # echo the command and not exec
-     DEBUG_ECHO=1
+  d)	# not distrib certs
+    __DIST=0
   ;;
-  d) # Force Distribute
-     FORCE_DIST=1
+  f)	# Force
+    __FORCE=1
   ;;
-  o) # Distribute Only
-     DIST_ONLY=1
-     FORCE_DIST=1
+  r)	# Dry-Run
+    __ACME_ACTION="--renew"
   ;;
-  u) # Force Update
-     FORCE_UPDATE=1
+  t)	# Dry-Run
+    __TEST=1
   ;;
-  *) # Default
-     echo "${0}: Error."
-     echo "Usage: ${0} [-DEdou]"
-     echo "    -D: DEBUG print"
-     echo "    -E: Echo only to execute command"
-     echo "    -d: Force Distribute"
-     echo "    -o: Distribute Certificate file without update"
-     echo "    -u: Force Update"
-     exit 1
+  V)	# Verboce/Debug
+    __DBG=1
+    __LOG="`basename ${0}`.log"
+    [ -f ${__LOG} ] && rm ${__LOG} && touch ${__LOG}
+  ;;
+  C)	# Config file
+    __CONF_TMP=${OPTARG}
+  ;;
+  D)	# Domains file
+    __DOMF_TMP=${OPTARG}
+  ;;
+  H)	# Home Directory
+    __HOME_TMP=${OPTARG}
+  ;;
+  W)	# WebRoot Directory
+    __WEBR_TMP=${OPTARG}
+  ;;
+  *)
+    cu_usage
+    exit 1
   ;;
   esac
 done
 
-################################################################################
-# Initialize
-################################################################################
+[ "${__ACME}" -eq 0 -a "${__DIST}" -eq 0 ] && echo "done." && exit 0
 
-TAB=$'\t'
-ACME_OPTS="-bnN"
-PATH="/usr/local/bin:${PATH}"
-
-ECHO=""; [ ${DEBUG_ECHO} -ne 0 ] && ECHO="/bin/echo"
-ACCKEY="${ACME_BASE}/SSL/privkey.pem"
-SSL_DIR="${ACME_BASE}/SSL"
-CHALLENGE="${ACME_BASE}/htdocs"
-DOMAINSFILE="${ACME_BASE}/domains.txt"
-LE_ERRORED_DOMAIN=""
-DI_ERRORED_DOMAIN=""
-SANTMP=`mktemp /tmp/san.XXXXXXXX` || exit 1
-TEMPFILE=`sudo -u ${DUSER} mktemp /tmp/crt.XXXXXXXX` || exit 1
-RMTCMDS=`sudo -u ${DUSER} mktemp /tmp/crt.XXXXXXXX` || exit 1
-
-[ ${DEBUG} -ne 0 ] && ACME_OPTS="${ACME_OPTS} -v"
-[ ${FORCE_UPDATE} -ne 0 ] && ACME_OPTS="${ACME_OPTS} -F"
-CUR_UID=`id -u`
-
-if [ ${DEBUG} -ne 0 ]; then
-  echo "----- DEBUG -----"
-  echo "ACME_OPTS=${ACME_OPTS}"
-  echo "EXEC UID=${CUR_UID}"
-  echo "ECHO=\"${ECHO}\""
-  echo "===== DEBUG end ====="
+if [ "${__DBG}" -ne 0 ]; then
+  cu_debug "TEST    =${__TEST}"
+  cu_debug "ACME    =${__ACME}"
+  cu_debug "DIST    =${__DIST}"
+  cu_debug "FORCE   =${__FORCE}"
 fi
 
-[ ${CUR_UID} -ne 0 ] && echo "Must run by root/UID=0" && exit
-[ ! -d ${ACME_BASE} ] && echo "${ACME_BASE} dir is not exist" && exit
-[ ! -e ${DOMAINSFILE} ] && echo "Error: Dose not exist ${DOMAINSFILE}" && exit 1
+### Parse conf.
 
-for i in ${SSL_DIR} ${CHALLENGE}; do
-  if [ ! -d ${i} ]; then
-    mkdir ${i} || (echo "Error: Did not make ${i}" && exit 1)
-  fi
-done
+[ -n "${__HOME_TMP}" ] && __HOME="${__HOME_TMP%/}"
+[ -n "${__HOME_TMP}" ] && __CONF="${__HOME}/certupdate.conf"
+[ -n "${__CONF_TMP}" ] && __CONF="${__CONF_TMP}"
+[ ! -f "${__CONF}" ] && echo "Error: ${__CONF} is not found" && cu_usage
+. ${__CONF}
+[ ${?} -ne 0 ] && echo "Error: Configuration file ${__CONF} is somthing wrong. check it." && exit 1
 
-################################################################################
-# Main routine
-################################################################################
+[ -n "${__HOME_TMP}" ] && __HOME=${__HOME_TMP%/} || __HOME=${PWD%/}
+[ -n "${__DOMF_TMP}" ] && __DOMF="${__DOMF_TMP}"
+[ -n "${__WEBR_TMP}" ] && __WEBR="${__WEBR_TMP}"
+__WORK="${__HOME}/CERTS"
 
-# Main loop
-cat ${DOMAINSFILE} | while read DOMS_LINE ; do
-  DOMAIN=`echo ${DOMS_LINE} | sed 's/[#|].*$//'`
-  [ -z ${DOMAIN} ] && continue
+[ -f ${__WORK}/acme.sh.log ] && rm ${__WORK}/acme.sh.log
 
-  # Get CERT file from Let's Encrypt by acme-client
+if [ "${__DBG}" -ne 0 ]; then
+  cu_debug "CONF_TMP=${__CONF_TMP}"
+  cu_debug "CONF    =${__CONF}"
+  cu_debug "DOMF_TMP=${__DOMF_TMP}"
+  cu_debug "DOMF    =${__DOMF}"
+  cu_debug "HOME_TMP=${__HOME_TMP}"
+  cu_debug "HOME    =${__HOME}"
+  cu_debug "WEBR_TMP=${__WEBR_TMP}"
+  cu_debug "WEBR    =${__WEBR}"
+  cu_debug "WORK    =${__WORK}"
+  cu_debug "USER   =${__DUSER}"
+fi
 
-  echo ""
-  echo "Getting ${DOMAIN} Certificates"
-  DOMKEY=${SSL_DIR}/${DOMAIN}/privkey.pem
-  [ ! -d ${SSL_DIR}/${DOMAIN} ]   && ${ECHO} mkdir ${SSL_DIR}/${DOMAIN}
+[ ! -f "${__DOMF}" ] && echo "Error: ${__DOMF} is not found" && cu_usage
+[ ! -d "${__HOME}" ] && echo "Error: ${__HOME} is not found" && cu_usage
+[ ! -r "${__HOME}" -o ! -w "${__HOME}" -o ! -x "${__HOME}" ] \
+	&& echo "Error: permission denid ${__HOME} " && exit 1
 
-  # Get Subject Alternative Names
-  SAN=""
-  SANO=0
-  SANC=0
-  echo "" > ${SANTMP}
-  if [ -e "${ACME_BASE}/${DOMAIN}" ]; then
-    for i in `cat ${ACME_BASE}/${DOMAIN} | sed -E -e 's/#.*$//' -e "s/[ ${TAB}]+$//"`; do
-      SAN="${SAN} ${i}"
-      echo ${i} >> ${SANTMP}
-    done
-    SANO=`cat ${SANTMP} | sed -E -e '/^$/d' | wc -l | awk '{print $1}'`
-    SANC=`cat ${SANTMP} | sed -E -e '/^$/d' | sort | uniq | wc -l | awk '{print $1}'`
-    [ ${DEBUG} -ne 0 ] && echo "SANC=${SANC}, SANO=${SANO}, SAN=${SAN}"
-  fi
+if [ ! -e "${__WEBR}" ]; then
+  mkdir ${__WEBR}
+  [ ${?} -ne 0 ] && echo "Error: Cannot make ${__WEBR} directory." && exit 1
+  ln -s ${__WEBR} ${__WEBR}/.well-known
+  ln -s ${__WEBR} ${__WEBR}/acme-challenge
+  cat << __END__ > ${__WEBR}/index.html
+<HTML>
+<BODY>
+</BODY>
+<CENTER><H1> Not public </H1></CENTER>
+<HR>
+<P>
+This page is not public. Bye!!
+</P>
+</HTML>
+__END__
+elif [ ! -d "${__WEBR}" ]; then
+  echo "Error: ${__WEBR} is not directory." && exit 1
+elif [ ! -r "${__WEBR}" -o ! -w "${__WEBR}" -o ! -x "${__WEBR}" ]; then
+  echo "Error: permission denid ${__WEBR} " && exit 1
+fi
 
-  if [ ${DIST_ONLY} -eq 0 ]; then
-    if [ ${SANC} -eq 0 ]; then
-      ${ECHO} /usr/local/bin/acme-client ${ACME_OPTS} -k ${DOMKEY} -f ${ACCKEY} -C ${CHALLENGE} -c ${SSL_DIR}/${DOMAIN} ${DOMAIN}
-      RESULT=$?
-    else
-      if [ ${SANO} -ne ${SANC} ]; then
-        echo "Error: ${DOMAIN}: some SAN duplicates."
-        RESULT=1
-      else
-        if [ ${SANC} -gt 100 ]; then
-          echo "Error: ${DOMAIN}: SAN is defineded more than 100. Do not update certs."
-          RESULT=1
-        else
-          ${ECHO} /usr/local/bin/acme-client ${ACME_OPTS} -k ${DOMKEY} -f ${ACCKEY} -C ${CHALLENGE} -c ${SSL_DIR}/${DOMAIN} ${SAN}
-          RESULT=$?
-        fi
-      fi
+if [ ! -e "${__WORK}" ]; then
+  mkdir ${__WORK}
+  [ ${?} -ne 0 ] && echo "Error: Cannot make ${__WORK} directory." && exit 1
+elif [ ! -d "${__WORK}" ]; then
+  echo "Error: ${__WORK} is not directory." && exit 1
+elif [ ! -r "${__WORK}" -o ! -w "${__WORK}" -o ! -x "${__WORK}" ]; then
+  echo "Error: permission denid ${__WORK} " && exit 1
+fi
+
+# XXX temporaly XXX
+sudo service nginx restart > /dev/null 2>&1
+# XXX temporaly XXX
+
+### Parse domain.
+
+while read __LINE__; do
+
+  __UPD=0
+  __DOM=`echo "${__LINE__}" | sed -e "s/^[ ${__TAB}]+//" -e "s/[#|].*$//" -e "s/[ ${__TAB}]*$//"`
+  [ -z "${__DOM}" ] && continue
+
+  echo "Get ${__DOM} Certificate from ${__CAI}"
+
+  # Check SAN(Subject Alternative Name)
+  __SAN__="";__SAN_O__=0;__SAN_C__=0
+  if [ -e "${__HOME}/SAN/${__DOM}" ]; then
+    [ "${__DBG}" -ne 0 ] && cu_debug "SAN found."
+    while read i; do
+      __i=`echo "${i}"   | sed -e "s/^[ ${__TAB}]+//" -e "s/[#].*$//" -e "s/[ ${__TAB}]*$//"`
+      [ -z "${__i}" ] && continue
+      __SAN__="${__SAN__} ${__i}"
+    done < ${__HOME}/SAN/${__DOM}
+
+    __SAN_O__=`cu_count_arg ${__SAN__}`
+    __SAN_C__=`cu_sort_uniq_arg ${__SAN__}`
+
+    if [ "${__DBG}" -ne 0 ]; then
+      cu_debug "SAN_O=${__SAN_O__}."
+      cu_debug "SAN_C=${__SAN_C__}."
+      cu_debug "SAN=${__SAN__}."
+    fi
+
+    if [ "${__SAN_O__}" -ne "${__SAN_C__}" ]; then
+      echo "Error: ${__DOM}: some SAN entry is duplicated. ${__DOM} is skipped."
+    [ "${__DBG}" -ne 0 ] && cu_debug "Error: ${__DOM}: some SAN entry is duplicated. ${__DOM} is skipped."
+      continue
+    elif [ "${__SAN_O__}" -gt 100 ]; then
+      echo "Error: ${__DOM}: SAN entry is more than 100. ${__DOM} is skipped."
+      [ "${__DBG}" -ne 0 ] && cu_debug "Error: ${__DOM}: SAN entry is more than 100. ${__DOM} is skipped."
+      continue
     fi
   fi
 
-  [ ${FORCE_DIST} -ne 0 ] && RESULT=0
+  [ -z "${__SAN__}" ] && __SAN__="${__DOM}"
+  __SAN__="-d `echo ${__SAN__} | sed -e 's/^[[:space:]]//' -e 's/ / -d /g'`"
+  [ "${__DBG}" -ne 0 ] && cu_debug "SAN=${__SAN__}."
 
-  # Distribute Certificate file to target machines.
+  # issue or renew certs.
+  if [ "${__ACME}" -ne 0 ]; then
 
-  case ${RESULT} in
-    0) echo "${DOMAIN} is updated. Transfer key files to servers."
-       TARGET=`echo ${DOMS_LINE} | sed 's/.*|//' | sed 's/[\t ]*#.*$//'`
-       [ ${DEBUG} -ne 0 ] && echo "DOMAIN: ${DOMAIN}" && echo "TARGET: ${TARGET}"
-       [ -z "${TARGET}" ] && echo "${DOMAIN} has no target host" && continue
+    __ACME_OPTS__="${__ACME_ACTION} ${__SAN__} --home ${__WORK} --log ${__WORK}/acme.sh.log"
+    __ACME_OPTS_WEB__="--webroot ${__WEBR}"
+    __ACME_OPTS_CF__=""
 
-       for j in ${TARGET}; do
-         TARGETADDR=${j%%:/*}; [ ${DEBUG} -ne 0 ] && echo "TargetAddr: ${TARGETADDR}"
-         TARGETDIR=${j##*:};   [ ${DEBUG} -ne 0 ] && echo "TargetDitr: ${TARGETDIR}"
-         echo ${TARGETADDR} >> ${TEMPFILE}
-         ${ECHO} chmod 444 ${SSL_DIR}/${DOMAIN}/privkey.pem
-         ${ECHO} sudo -u ${DUSER} scp -q -rp ${SSL_DIR}/${DOMAIN}/fullchain.pem ${TARGETADDR}:${DOMAIN}.cert
-         ${ECHO} sudo -u ${DUSER} scp -q -rp ${SSL_DIR}/${DOMAIN}/privkey.pem   ${TARGETADDR}:${DOMAIN}.key
+    [ "${__FORCE}" -ne 0 ] && __ACME_OPTS_CF__="${__ACME_OPTS_CF__} -f"
+    [ "${__TEST}" -ne 0 ]  && __ACME_OPTS_CF__="${__ACME_OPTS_CF__} --test"
 
-         ########################################
-         echo "#! /bin/sh" > ${RMTCMDS}
-         cat << __END_OF_FILE__ | egrep -v '(^#|^$)' >> ${RMTCMDS}
-#!/bin/sh
-###############################################################################
-[ ! -e ${TARGETDIR} ] && mkdir ${TARGETDIR} && chmod 755 ${TARGETDIR}
-[ ! -d ${TARGETDIR} ] && echo "Error!!! ${TARGET} is exist but not Directory."
+    acme.sh ${__ACME_OPTS__} ${__ACME_OPTS_CF__} ${__ACME_OPTS_WEB__} 2>&1 > /dev/null
+    case "${?}" in
+    0)	# Issued certificate.
+      echo "${__DOM} certificate file is renewd."
+      __UPD=1
+    ;;
+    1)	# Error
+      echo "Error: acme.sh exits with errors in ${__DOM}. exec this with -V and check LOG."
+      [ "${__DBG}" -ne 0 ] && cu_debug "Error: acme.sh exits with errors in ${__DOM}. exec this with -V and check LOG."
+      continue
+    ;;
+    2)	# Skip to issue/renew certificate.
+      echo "${__DOM} not changed. Skip it."
+      __UPD=0
+    ;;
+    *)	# Unknown result.
+      echo "Error: acme.sh exits with unknown error."
+      [ "${__DBG}" -ne 0 ] && cu_debug "Error: acme.sh exits with unknown error."
+      continue
+    ;;
+    esac
+  fi
 
-mv ${DOMAIN}.cert ${DOMAIN}.key ${TARGETDIR}
-chown root:wheel ${TARGETDIR}/${DOMAIN}.cert ${TARGETDIR}/${DOMAIN}.key
-chmod 400 ${TARGETDIR}/${DOMAIN}.key
+  # distribute new cert files.
+  if [ "${__DIST}" -ne 0 ]; then
 
+    # Not update and not force distribute, skip to distribute cert files.
+    [ "${__UPD}" -eq 0 -a ${__FORCE} -eq 0 ] && continue
+
+    CU_SSH_OPT="-q -o ControlMaster=auto -o ControlPath=/tmp/acme-%r@%h:%p -o ControlPersist=5"
+    __TGT=`echo "${__LINE__}" | sed -e "s/[^|]*|//" -e "s/[#].*$//" -e "s/[ ${__TAB}]*$//"`
+    [ "${__DBG}" -ne 0 ] && cu_debug "DOMAIN: ${__DOM}" && cu_debug "TARGET: ${__TGT}"
+    [ -z "${__TGT}" ] && echo "${__DOM} has no target host" && continue
+
+    for i in ${__TGT}; do
+      __TGTADDR=${i%%:/*}; [ ${__DBG} -ne 0 ] && cu_debug "TargetAddr: ${__TGTADDR}"
+      __TGTDIR=${i##*:};   [ ${__DBG} -ne 0 ] && cu_debug "TargetDir:  ${__TGTDIR}"
+
+      # open ssh connection with Control master
+      sudo -u ${__DUSER} ssh ${CU_SSH_OPT} -N -f ${__TGTADDR}
+
+      # Execute
+      # Create TEMP Dir
+      __TMPDIR=`sudo -u ${__DUSER} ssh -n ${CU_SSH_OPT} ${__TGTADDR} mktemp -d`
+
+      sudo -u ${__DUSER} scp ${CU_SSH_OPT} ${__WORK}/${__DOM}/fullchain.cer ${__TGTADDR}:${__TMPDIR}/${__DOM}.cert
+      sudo -u ${__DUSER} scp ${CU_SSH_OPT} ${__WORK}/${__DOM}/${__DOM}.key  ${__TGTADDR}:${__TMPDIR}/${__DOM}.key
+
+      cat << __END__ | ( sudo -u ${__DUSER} ssh ${CU_SSH_OPT} ${__TGTADDR} "cat - | /bin/sh" ) >> ${__LOG}
+sudo chown root:wheel ${__TMPDIR}/${__DOM}.cert ${__TMPDIR}/${__DOM}.key
+sudo chmod 444        ${__TMPDIR}/${__DOM}.cert
+sudo chmod 400        ${__TMPDIR}/${__DOM}.key
+sudo mv ${__TMPDIR}/${__DOM}.cert ${__TMPDIR}/${__DOM}.key ${__TGTDIR}
 for i in nginx postfix dovecot; do
-  which \${i};   [ \$? -eq 0 ] && echo "restart \${i}" && service \${i} restart
+  which \${i} > /dev/null 2>&1
+  [ \$? -eq 0 ] && echo "restart \${i}" && sudo service \${i} restart 2>&1
 done
-##### Post execute.
-rm ./rmtcmds.sh
-###############################################################################
-__END_OF_FILE__
+__END__
+# ***** WARNING *****
+# mktemp -d is not defined on POSIX but many of OS has mktemp -d.
+# This will be removed and replace to 
+#     https://github.com/ShellShoccar-jpn/misc-tools/blob/master/mktemp
+# Reload certificate file routine is temporaly impremented. This is hardcoded and agry.
+# This must be rewrite with configurable.
+# ***** WARNING *****
 
-         ${ECHO} sudo -u ${DUSER} scp -q ${RMTCMDS} ${TARGETADDR}:rmtcmds.sh
-         ${ECHO} rm ${RMTCMDS}
-         ROOTCMD="/bin/sh rmtcmds.sh"
-         ${ECHO} sudo -u ${DUSER} ssh -qtn ${TARGETADDR} sudo ${ROOTCMD}
-       done
-       echo "${DOMAIN} done."
-       ;;
-    1) echo "${DOMAIN} is troubled. Check please."
-       LE_ERRORED_DOMAIN="${LE_ERRORED_DOMAIN}, ${DOMAIN}:${RESULT}"
-       ;;
-    2) echo "${DOMAIN} doesn't need to update"
-       ;;
-    *) echo "Unknown status of acme-client. Cehck please."
-       LE_ERRORED_DOMAIN="${LE_ERRORED_DOMAIN}, ${DOMAIN}:${RESULT}"
-       ;;
-  esac
-done
-[ ! -z ${LE_ERRORED_DOMAIN} ] && echo "acme-client errored domain:" && echo "${LE_ERRORED_DOMAIN}"
-[ ! -z ${DI_ERRORED_DOMAIN} ] && echo "distribute errored domain:" && echo "${DI_ERRORED_DOMAIN}"
+      # Postpone
+      sudo -u ${__DUSER} ssh         -n ${CU_SSH_OPT} ${__TGTADDR} rm -rf ${__TMPDIR}
+      sudo -u ${__DUSER} ssh -O exit ${CU_SSH_OPT} ${__TGTADDR}
+    done
+  fi
+  echo "${__DOM} is done."
+done < ${__DOMF}
